898873ca1a214b1811db1a5c4f65ab2338b4f58c
tolgayonter <tolgayonter@gmail.com>
2023-03-10 10:37:58 +0300

    add basic structure

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl('')
+  })
+
+}
