3e4a6b53dcde3b63638c2ebf847a3bd8cfbb624c
tolgayonter <tolgayonter@gmail.com>
2023-04-20 17:51:59 +0300

    add emails index

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: ':id', component: EmailShowComponent },
+      { path: '', component: PlaceholderComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class InboxRoutingModule { }
+export class InboxRoutingModule {}
