e689a5ed0031ffdd9e905988ca6adb6f2559e42a
tolgayonter <tolgayonter@gmail.com>
2023-04-21 18:12:26 +0300

    add email reply

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent {
+  showModal = false;
+  @Input() email!: Email;
 
+  constructor(private emailService: EmailService) {}
+
+  ngOnInit() {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `re: ${this.email.subject}`,
+      text: `\n\n\n-------- ${this.email.from} wrote:\n> ${text}`,
+    };
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
